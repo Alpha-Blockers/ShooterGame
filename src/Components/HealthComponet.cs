dcd3364b87a0b41db7e9c8f135e613c689f6e7f8
paarii <101456989@student.swin.edu.au>
2018-05-16 13:48:51 +1000

    Fixed the attack and health component

--- src/Components/HealthComponet.cs
+++ src/Components/HealthComponet.cs
@@ -5,20 +5,53 @@ using System.Text;
 
 namespace ShooterGame
 {
-    class HealthComponet
+    class HealthComponet : IComponent
     {
-        public int _health;
+        private int _health;
+        private Entity _entity;
 
+        /// <summary>
+        /// Get or set parent entity.
+        /// </summary>
+        public Entity Entity
+        {
+            get => _entity;
+            set
+            {
+                if (_entity != value)
+                {
+                    if (_entity != null) _entity.Health = null;
+                    _entity = value;
+                    if (_entity != null) _entity.Health = this;
+                }
+            }
+        }
 
+        /// <summary>
+        ///  get or set HEalth Value
+        /// </summary>
         public int Health
         {
             get => _health;
             set { _health = value; }
         }
 
+
+        /// <summary>
+        /// Health component cunstructer
+        /// </summary>
+        /// <param name="health">initial health value(optional)</param>
         public HealthComponet(int health = 100)
         {
             _health = health;
+        }
+
+        /// <summary>
+        ///  Clear all component data and attempt to unlink from any external data.
+        /// </summary>
+        public void Destroy()
+        {
+            //empyty
         }
 
     }
